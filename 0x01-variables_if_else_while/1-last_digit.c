1354f0e129574c8d587fd80387b73bd1425ac885
Jeffrey Kalei <jeffreykalei@gmail.com>
2023-09-10 08:18:49 -0700

    Change code to betty compliant

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/** 
- * main - Entry point 
+/**
+ * main - Entry poi
  * Return 0 (Success)
 */
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-	
-	int m; 
+
+	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -20,7 +20,7 @@ int main(void)
 	if (m > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	if (m == 0)
-		printf ("Last digit of %d is %d and is 0\n", n, m);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
